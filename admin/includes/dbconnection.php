981aa4eef0d819c2bf2b131ac5bc799114d43b2b
karthikp196 <karthikp196@gmail.com>
2019-08-28 13:16:49 +0530

    first commit

---
+++ admin/includes/dbconnection.php
@@ -0,0 +1,6 @@
+<?php
+$con=mysqli_connect("Localhost", "fixxgqip_emp", "fixx@123#", "fixxgqip_employee_management");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+?>
